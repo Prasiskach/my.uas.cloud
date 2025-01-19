57d0d32c416419f1569d873461a8580ea29bdef9
Adhi Purnawan <157178968+adhinstiki@users.noreply.github.com>
2025-01-20 02:05:11 +0800

    Update submit.php

--- submit.php
+++ submit.php
@@ -103,12 +103,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } catch (AwsException $e) {
         $_SESSION['error_message'] = "Error uploading file to S3: " . $e->getMessage();
         // Alert dengan pesan error
-        echo "<script>alert('Error uploading file to S3: " . $e->getMessage() . "');</script>";
+        header("Location: index.php?status=error-upload-file");
         exit();
     } catch (PDOException $e) {
         $_SESSION['error_message'] = "Error inserting data into database: " . $e->getMessage();
         // Alert dengan pesan error
-        echo "<script>alert('Error inserting data into database: " . $e->getMessage() . "');</script>";
+        header("Location: index.php?status=error-insert-data");
         exit();
     }
     
