d4439e36d38c0d4bf83ce2535b4095b43c19c8d6
adhinstiki <157178968+adhinstiki@users.noreply.github.com>
2025-01-20 12:06:41 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
-$host = 'database-2.cluster-c52gqk4oa45c.us-east-1.rds.amazonaws.com';  
-$user = 'dbkampyus';      
+$host = 'localhost';  
+$user = 'root';      
 $password = 'Batanta71'; 
 $db_name = 'administrasi'; 
 $charset = 'utf8mb4';
